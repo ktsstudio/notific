5ad9c544d85242919c55467f3d1678acfd8e62f5
George Biryukov <g.birjukov@corp.mail.ru>
2014-11-07 10:17:19 +0300

    css classes names update

--- src/notific.js
+++ src/notific.js
@@ -1,26 +1,27 @@
 define(['jquery'], function ($) {
     'use strict';
 
-    var notify = {
+    var notific = {
         _default: {
             title: 'Notification',
             text: '',
             type: 'default',
-            timeout: 5000,
+            timeout: false,
             position: 'bottom'
         },
         _params: {
             body: 'body',
-            container: 'notify',
-            notification: 'notify__alert',
-            title: 'notify__title',
-            text: 'notify__text'
+            container: 'notific',
+            notification: 'notific__alert',
+            title: 'notific__title',
+            text: 'notific__text',
+            close: 'notific__close'
         },
         _types: {
-            default: 'notify__alert_info',
-            error: 'notify__alert_danger',
-            success: 'notify__alert_success',
-            warning: 'notify__alert_warning'
+            default: 'notific__alert_info',
+            error: 'notific__alert_danger',
+            success: 'notific__alert_success',
+            warning: 'notific__alert_warning'
         },
         _init: function () {
             var self = this,
@@ -28,28 +29,30 @@ define(['jquery'], function ($) {
 
             $(this._params.body).append($container);
 
-            $container.on('click', '.notify__close', function () {
+            $container.on('click', '.' + this._params.close, function () {
                 var $notification = $(this).closest('.' + self._params.notification);
                 self.close($notification);
             });
 
             return $container;
         },
-        _verifyNotificationType: function (options) {
-            if (typeof(this._types[options.type]) === 'undefined') {
+        _verifyNotificationType: function (type) {
+            if (typeof(this._types[type]) === 'undefined') {
                 return 'default';
             } else {
-                return options.type;
+                return type;
             }
         },
         _scrollToBottom: function ($container) {
             $container.stop().animate({ scrollTop: $container.prop('scrollHeight')}, 300);
         },
+        /*
         _styleBootstrap: function ($notification) {
             if ($notification.css('border-width') === '0px') {
-                $notification.addClass('notify__alert_with-shadow');
+                $notification.addClass('notific__alert_with-shadow');
             }
         },
+         */
         show: function (opts) {
             var $container = $('.' + this._params.container);
             if ($container.length === 0) {
@@ -59,27 +62,27 @@ define(['jquery'], function ($) {
             var options = {};
             $.extend(options, this._default, opts);
 
-            options.type = this._verifyNotificationType(options);
+            options.type = this._verifyNotificationType(options.type);
 
             var $notification = $(
                 '<div class="' + this._params.notification + ' ' + this._types[options.type] + '">' +
-                    '<button type="button" class="notify__close">&times;</button>' +
+                    '<button type="button" class="' + this._params.close + '">&times;</button>' +
                     '<h4 class="' + this._params.title + '">' + options.title + '</h4>' +
                     '<p class="' + this._params.text + '">' + options.text + '</p>' +
-                    '</div>'
+                '</div>'
             );
 
             $container.append($notification);
-            this._styleBootstrap($notification);
+            //this._styleBootstrap($notification);
             this._scrollToBottom($container);
 
             //todo плохо, отрефакторить, ибо изначально в архитектуре этого нет
             if (options.position === 'top') {
-                $container.removeClass('notify_bottom');
-                $container.addClass('notify_top');
+                $container.removeClass('notific_bottom');
+                $container.addClass('notific_top');
             } else {
-                $container.removeClass('notify_top');
-                $container.addClass('notify_bottom');
+                $container.removeClass('notific_top');
+                $container.addClass('notific_bottom');
             }
 
             if (options.timeout) {
@@ -91,23 +94,19 @@ define(['jquery'], function ($) {
 
             return $notification;
         },
-
         error: function (opts) {
             return this.show($.extend(opts, {type: 'error'}));
         },
-
         success: function (opts) {
             return this.show($.extend(opts, {type: 'success'}));
         },
-
         warning: function (opts) {
             return this.show($.extend(opts, {type: 'warning'}));
         },
-
         close: function ($notification) {
             $notification.remove();
         }
     };
 
-    return notify;
+    return notific;
 });
